e71528e680610835934cd29c4c2dd1723ee28e72
EvanBoettner <114521911+EvanBoettner@users.noreply.github.com>
2023-03-19 14:37:04 -0500

    Error handling and app completion
    Added error handling on the server side and frontend side.

--- dreamApp/main.js
+++ dreamApp/main.js
@@ -1,24 +1,47 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector("form");
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener("submit", async (e) => {
+  showSpinner();
+  e.preventDefault(); //When a form is submitted it refreshes the entire page, we want to prevent that
+
+  const data = new FormData(form); //We want to extract the data from the form so we instantiate a new FormData object using the form as the input
+  //This will provide us with a data structure that behaves like a javascript map
+
+  const response = await fetch("http://localhost:8080/dream", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", //since we are using a jsonAPI we want a header declaring that
+    },
+    body: JSON.stringify({
+      //We are wrapping it in JSON.stringify in order to convert it into a string
+      prompt: data.get("prompt"), //we are grabbing the prompt from the form using the get method
+    }),
+  });
+
+  if (response.ok) {
+    const { image } = await response.json(); //We are accessing the image by awaiting the response.json()
+
+    const result = document.querySelector("#result");
+    result.innerHTML = `<img src="${image}" width="512" />`; //Here we are inserting the image url recieved from the api
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+  }
+
+  hideSpinner();
+});
+
+function showSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = true; //While the api is loading we don't want users sending multiple forms so we disable the button
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>';
+}
+
+function hideSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = false;
+  button.innerHTML = "Dream";
+}
--- dreamApp/server.js
+++ dreamApp/server.js
@@ -1,37 +1,44 @@
-import * as dotenv from 'dotenv';
-dotenv.config() //Allows us to access our enviornment variables defined in our .env file
+import * as dotenv from "dotenv";
+dotenv.config(); //Allows us to access our enviornment variables defined in our .env file
 
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI, //Access the openai key from the env file
-})
+  apiKey: process.env.OPENAI, //Access the openai key from the env file
+});
 
 const openai = new OpenAIApi(configuration); //initializes the openai sdk
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const app = express(); //Allows us to use various different middleware 
-app.use(cors()); //cross orgin resource sharing / a security mechanism 
+const app = express(); //Allows us to use various different middleware
+app.use(cors()); //cross orgin resource sharing / a security mechanism
 app.use(express.json()); //This well tell our api that it only wants to handle incoming data in a json format
 
-app.post('/dream', async (req, res) => {
-
+app.post("/dream", async (req, res) => {
+  try {
     const prompt = req.body.prompt; //Accessing the description of the image the user is trying to generate
 
-    const aiResponse = await openai.createImage({ //Using await on this call will pause the execution until openAi is finished
-        prompt,
-        n: 1,//number of images 
-        size: '1024x1024',//resolution size
+    const aiResponse = await openai.createImage({
+      //Using await on this call will pause the execution until openAi is finished
+      prompt,
+      n: 1, //number of images
+      size: "1024x1024", //resolution size
     }); // Passing the prompt to the openai api by calling the createImage method and passing the prompt as an argument
 
     const image = aiResponse.data.data[0].url; // uses the aiResponse to get the image url
     res.send({ image }); //sends the image back to the client as a response. will be recieved as json data
-}); 
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
 //This is our first endpoint, we are using post here because we are creating a new piece of data
 // The post method takes two arguments, the first argument is a string that reperesents the URL of the API
 // The second argument is a callback function that has a request and response object that we can interact with
 // The callback function will be called everytime someone navigates to this url
 
-app.listen(8080, () => console.log('make art on http://localhost:8080/dream')); //fires up the app using the port you want to use 
+app.listen(8080, () => console.log("make art on http://localhost:8080/dream")); //fires up the app using the port you want to use
